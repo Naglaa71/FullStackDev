ad88c1d6cef1789d5afdac05c65e8bdbaf7056e5
Naglaa Awad <Naglaa Awad>
2016-09-15 11:52:41 -0400

    Initial commit to my first spring AtoZ project

---
+++ src/main/java/com/atozwebproject/AtozwebprojectApplication.java
@@ -0,0 +1,12 @@
+package com.atozwebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtozwebprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtozwebprojectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/atozwebproject/AtozwebprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atozwebproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AtozwebprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
