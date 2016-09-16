356e11c29f11eb44be7bbe61762580c9a10ceab9
naglaa awad <naglaaawad71@yahoo.com>
2016-09-16 12:54:26 -0400

    Applied the logger framework slf4j and tested through I18NService class

--- src/main/java/com/fullstackdev/web/i18n/I18NService.java
+++ src/main/java/com/fullstackdev/web/i18n/I18NService.java
@@ -1,17 +1,22 @@
 package com.fullstackdev.web.i18n;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Locale;
+import org.slf4j.Logger;
 
 /**
  * Created by nagla on 9/16/2016.
  */
 @Service
 public class I18NService {
+    //application Logger
+    private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
+
     @Autowired
     private MessageSource messageSource;
     /**
@@ -19,6 +24,7 @@ public class I18NService {
      * @param messageId The key to the messages resource file
      **/
     public String getMessage(String messageId) {
+        LOG.info(" My message is {}", messageId);
         Locale locale = LocaleContextHolder.getLocale();
         return getMessage(messageId,locale);
     }
