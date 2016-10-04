ee9544330b7cfc9465b93a8ecd7d4ce3921ad02d
naglaa awad <naglaaawad71@yahoo.com>
2016-10-04 15:32:50 -0400

    Encrypted the password using spring passwordEncoder class

--- src/main/java/com/fullstackdev/config/SecurityConfig.java
+++ src/main/java/com/fullstackdev/config/SecurityConfig.java
@@ -2,13 +2,16 @@ package com.fullstackdev.config;
 
 import com.fullstackdev.backend.service.UserSecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,6 +21,13 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String SALT="dflsdjfksdfksjdkfs000.,";
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12,new SecureRandom(SALT.getBytes()));
+    }
 
     @Autowired
     private UserSecurityService userSecurityService;
@@ -58,6 +68,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .userDetailsService(userSecurityService);
+                .userDetailsService(userSecurityService)
+                .passwordEncoder(passwordEncoder());
     }
 }
